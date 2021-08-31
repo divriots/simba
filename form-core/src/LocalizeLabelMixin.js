0421e0ef97e6310cff3d7a3bedfa44af41fbd604
Joren Broekema <joren.broekema@gmail.com>
2021-08-31 19:59:26 +0000

    fix: allow default localized label to be overridden

--- form-core/src/LocalizeLabelMixin.js
+++ form-core/src/LocalizeLabelMixin.js
@@ -4,10 +4,42 @@ import '~/validation-feedback/simba-validation-feedback.js';
 
 export const LocalizeLabelMixinImplementation = (superclass) =>
   class extends LocalizeMixin(superclass) {
+    get label() {
+      return (
+        this.__label || (this._labelNode && this._labelNode.textContent) || ''
+      );
+    }
+
+    /**
+     * @param {string} newValue
+     */
+    set label(newValue) {
+      if (!this.__updatingLabelFromLocalize && newValue) {
+        this.__userUpdatedLabelImperatively = true;
+      }
+      const oldValue = this.label;
+      /** @type {string} */
+      this.__label = newValue;
+      this.requestUpdate('label', oldValue);
+    }
+
     async onLocaleUpdated() {
       super.onLocaleUpdated();
+      // Only update label with localize default label if
+      // user has not imperatively set label or declaratively through a slottable
+      if (
+        !this.__userUpdatedLabelImperatively &&
+        this._isPrivateSlot('label')
+      ) {
+        this._updateLocalizedLabel();
+      }
+    }
+
+    _updateLocalizedLabel() {
+      this.__updatingLabelFromLocalize = true;
       this.label = localize.msg(`${this.tagName.toLowerCase()}:defaultLabel`);
       this._updateFeedbackComponent(); // new label means new feedback message
+      this.__updatingLabelFromLocalize = false;
     }
   };
 
