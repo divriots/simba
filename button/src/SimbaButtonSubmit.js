a9ab19e1332ddda08415c80ea74eb04c11c30c4f
Joren Broekema <36734656+undefined@users.noreply.github.com>
2022-03-06 10:49:14 +0000

    feat: create simba-submit-button

---
+++ button/src/SimbaButtonSubmit.js
@@ -0,0 +1,4 @@
+import { LionButtonSubmit } from '@lion/button';
+import { SimbaButtonMixin } from './SimbaButtonMixin.js';
+
+export class SimbaButtonSubmit extends SimbaButtonMixin(LionButtonSubmit) {}
