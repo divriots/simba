b3a90cb821469aa46b21eda8deb440ca8c2f46ca
Mikhail Bashkirov <bashmish@gmail.com>
2022-03-28 17:42:09 +0200

    chore: use curly braces syntax for the function

--- doc-layout/src/template.js
+++ doc-layout/src/template.js
@@ -4,62 +4,64 @@ import { html, unsafeHTML } from '~/core';
 import './color-toggler';
 import logoSvg from './logo.svg?raw';
 
-export const docLayoutTemplate = (content, context) => html`
-  <style>
-    ${unsafeHTML(styles)} .logo {
-      color: #f59e0b;
-    }
-    .topbar {
-      width: 100%;
-      display: flex;
-      align-items: center;
-      justify-content: right;
-    }
-    @media screen and (min-width: ${breakpoints.lg}) {
+export const docLayoutTemplate = (content, context) => {
+  return html`
+    <style>
+      ${unsafeHTML(styles)} .logo {
+        color: #f59e0b;
+      }
       .topbar {
-        justify-content: unset;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: right;
       }
-    }
-    .story_padded {
-      line-height: initial;
-    }
-    .preview-story .story_padded {
-      background-color: var(--simba-bg-color);
-      color: var(--simba-text-color);
-    }
-    html.dark .preview-story .story_padded {
-      background-color: var(--simba-bg-color-dark);
-      color: var(--simba-text-color-dark);
-    }
-    .markdown-body {
-      padding: 0;
-      color: white;
-    }
-    .markdown-body code:not([class*='language-']),
-    .markdown-body tt {
-      background-color: transparent;
-      border: none;
-    }
-  </style>
-  <mdjs-layout
-    .context="${context}"
-    @color-scheme-change="${(event) => {
-      if (event.detail.colorScheme === 'dark') {
-        document.documentElement.classList.add('dark');
-        document.documentElement.setAttribute('theme', 'dark');
-      } else {
-        document.documentElement.classList.remove('dark');
-        document.documentElement.setAttribute('theme', 'light');
+      @media screen and (min-width: ${breakpoints.lg}) {
+        .topbar {
+          justify-content: unset;
+        }
       }
-    }}"
-  >
-    <div class="logo" slot="logo" aria-label="starter-simba">
-      ${unsafeHTML(logoSvg)}
-    </div>
-    <div class="topbar" slot="topbar">
-      Themes:&nbsp;
-      <color-toggler></color-toggler>
-    </div>
-    <div class="prose dark:prose-invert">${unsafeHTML(content)}</div>
-  </mdjs-layout>
-`;
+      .story_padded {
+        line-height: initial;
+      }
+      .preview-story .story_padded {
+        background-color: var(--simba-bg-color);
+        color: var(--simba-text-color);
+      }
+      html.dark .preview-story .story_padded {
+        background-color: var(--simba-bg-color-dark);
+        color: var(--simba-text-color-dark);
+      }
+      .markdown-body {
+        padding: 0;
+        color: white;
+      }
+      .markdown-body code:not([class*='language-']),
+      .markdown-body tt {
+        background-color: transparent;
+        border: none;
+      }
+    </style>
+    <mdjs-layout
+      .context="${context}"
+      @color-scheme-change="${(event) => {
+        if (event.detail.colorScheme === 'dark') {
+          document.documentElement.classList.add('dark');
+          document.documentElement.setAttribute('theme', 'dark');
+        } else {
+          document.documentElement.classList.remove('dark');
+          document.documentElement.setAttribute('theme', 'light');
+        }
+      }}"
+    >
+      <div class="logo" slot="logo" aria-label="starter-simba">
+        ${unsafeHTML(logoSvg)}
+      </div>
+      <div class="topbar" slot="topbar">
+        Themes:&nbsp;
+        <color-toggler></color-toggler>
+      </div>
+      <div class="prose dark:prose-invert">${unsafeHTML(content)}</div>
+    </mdjs-layout>
+  `;
+};
